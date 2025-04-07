4f2e6fd370c4c7336e3b533adec5f6f3bd353f57
JamesTzh <159073153+JamesTzh@users.noreply.github.com>
2025-04-07 10:04:51 +0800

    Updated port forwarding for docker intergration
    included port and host to allow local connection to docker's port

--- Task 1/src/Task1.py
+++ Task 1/src/Task1.py
@@ -3,5 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         reload = True,
-        app ="app:app"
+        app ="app:app",
+        host="0.0.0.0",
+        port=8000
     )
