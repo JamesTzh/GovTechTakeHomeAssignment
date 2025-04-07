d1715d7ae8da974782ab40e5e5614c890963cf6f
JamesTzh <159073153+JamesTzh@users.noreply.github.com>
2025-04-07 10:02:11 +0800

    Router includes FASTAPI's post and get
    Includes FASTAPI's post and get request that does: creating chat message, get user's chat messages, find user's chat, get all chats, send query, and delete chat.

---
+++ Task 1/src/router.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, HTTPException
+from typing import List
+from models import Message, MessageRequest
+from ai import format_history, get_ai_response
+from anonymizer import anonymize_username
+
+router = APIRouter()
+
+@router.post("/",response_model=Message)
+async def create_message(message_request: MessageRequest):
+    message_request.UserName = anonymize_username(message_request.UserName)
+    message = Message(**message_request.model_dump())
+    await message.create()
+    return message
+
+@router.get("/",response_model=List[Message])
+async def get_allmessages():
+    messages = await Message.find_all().to_list()
+    return messages
+
+@router.get("/{UserName}",response_model=List[Message])
+async def get_UserMessages(UserName:str):
+    UserName = anonymize_username(UserName)
+    messages = await Message.find(Message.UserName == UserName).to_list()
+    if not messages:
+        return []
+    return [MessageRequest(**message.model_dump()) for message in messages]
+
+@router.get("/{UserName}/{ChatTitle}",response_model=str)
+async def get_ChatMessage(UserName:str, ChatTitle:str):
+    UserName = anonymize_username(UserName)
+    message = await Message.find(Message.UserName == UserName and Message.ChatTitle == ChatTitle).first_or_none()
+    if not message:
+        return ""
+    return MessageRequest(**message.model_dump()).Message
+
+@router.put("/{UserName}/{ChatTitle}", response_model=str)
+async def update_post(UserName:str, ChatTitle:str, query: str):
+    UserName = anonymize_username(UserName)
+    message = await Message.find(Message.UserName == UserName and Message.ChatTitle == ChatTitle).first_or_none()
+    new = MessageRequest(UserName=UserName,ChatTitle=ChatTitle,Message=MessageRequest(**message.model_dump()).Message+","+"'"+query+"'")
+    history = format_history(MessageRequest(**new.model_dump()).Message)
+    await message.update({"$set":new.model_dump(exclude_unset = True)})
+    reply = get_ai_response(history)
+    new = MessageRequest(UserName=UserName,ChatTitle=ChatTitle,Message=MessageRequest(**message.model_dump()).Message+","+"'"+reply+"'")
+    await message.update({"$set":new.model_dump(exclude_unset = True)})
+    return reply
+
+@router.delete("/{message_id}")
+async def delete_message(message_id: str):
+    message = await Message.get(message_id)
+    throw_exception(message)
+    await message.delete()
+    return "Message deleted successfully"
+
+def throw_exception(message):
+    if not message:
+        raise HTTPException(status_code=404, detail="Message not found")
