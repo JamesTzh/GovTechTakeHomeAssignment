29e00c95b1c4da5ef23e1b33854ca161dd60cfc0
JamesTzh <159073153+JamesTzh@users.noreply.github.com>
2025-04-07 10:03:28 +0800

    Updated Lifespan
    Lifespan to wait for database's initialization

--- Task 1/src/app.py
+++ Task 1/src/app.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from contextlib import asynccontextmanager
+from database import init_db
+import router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router.router, prefix="/posts", tags=["Posts"])
 
